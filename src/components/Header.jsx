430a452faf81f839bbfab722e01b7a6a337a5d4a
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-01-30 01:42:37 +0530

    Corrected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <Card>
       <img src={image} id="header-img" />
-      <h1>Tic-Tac-Toe test commit to github</h1>
+      <h1>Tic-Tac-Toe</h1>
     </Card>
   );
 }
