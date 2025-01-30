30a029cb24c5170f3526399e81a096bef676c4dd
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-01-31 01:55:18 +0530

    More features

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Gameboard from "./components/Gameboard";
 import Header from "./components/Header";
 import Players from "./components/Players";
 import Card from "./Helpers/Card";
 
 function App() {
+
+  const [player, setPlayer] = useState('O');
+
+  const playerChangeHandler = () => {
+    if (player === 'O') {
+      setPlayer('X');
+    } else {
+      setPlayer('O');
+    }
+  }
+
   return (
       <>
       <Header />
       <Players />
-      <Gameboard />
+      <Gameboard activePlayer={player} onPlayerChange={playerChangeHandler}/>
       </>
   );
 }
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -7,16 +7,17 @@ const initialBoard = [
     [null, null, null]
 ];
 
-export default function Gameboard() {
+export default function Gameboard({activePlayer, onPlayerChange}) {
 
     const [board, setBoard] = useState(initialBoard);
 
     const buttonClickHandler = (row, col) => {
         setBoard((prev) => {
             const updatedBoard = [...prev.map((innerArray) => [...innerArray])];
-            updatedBoard[row][col] = 'O';
+            updatedBoard[row][col] = activePlayer;
             return updatedBoard;
         })
+        onPlayerChange();
     }
 
     return (
@@ -24,7 +25,7 @@ export default function Gameboard() {
             <div id="main-container">
                 {board.map((row, rowIndex) => <span key={rowIndex}>
                     <div className="inner-container">
-                        {row.map((item, colIndex) => <span key={colIndex} className="each-btn-span"><button onClick={() => buttonClickHandler(rowIndex, colIndex)} className="board-btn">{item}</button></span>)}
+                        {row.map((item, colIndex) => <span key={colIndex} className="each-btn-span"><button disabled={item !== null} onClick={() => buttonClickHandler(rowIndex, colIndex)} className="board-btn">{item}</button></span>)}
                     </div>
                 </span>)}
             </div>
